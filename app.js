93fc9b3c4a98cc6085bd488be24380c7898c5624
dexuiz <dex345z@gmail.com>
2017-02-08 00:21:52 +0530

    socket.io figured out

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express =require("express");
 var app=express();
 var server = require('http').createServer(app);
-var io = require("socket.io").listen(server);
+var io = require("socket.io")(server);
 app.set("view engine","ejs");
 
 var users=[];
@@ -11,17 +11,23 @@ app.get("/",function(req,res){
   res.render("index");
 })
 
-app.listen(process.env.port || 3000,function(){
+io.on('connection',function(socket){
+  connections.push(socket);
+  console.log(connections.length+"users connected");
+
+  socket.on("chat message",function(msg){
+    console.log("message"+msg);
+    io.emit("chat message",msg);
+  });
+
+
+
+  socket.on("disconnect",function(){
+    connections.splice(connections.indexOf(socket),1);
+    console.log(connections.length+"users connected");
+    });
+  });
+
+server.listen(process.env.port || 3000,function(){
   console.log("server started on port 3000");
 });
-
-io.sockets.on('connection',function(socket){
-  connections.push(socket);
-  console.log(connections.length+"sockets connected");
-
-  //disconnect
-  connections.splice(connections.indexOf(socket),1);
-  console.log(connections.length+"sockets connected");
-
-});
-console.log("is this working");
